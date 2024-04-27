4f505eccdfc15f443b06b211a52cdac8d6b4e3bb
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-04-27 19:27:01 +0530

    Create file

---
+++ 728-SelfDividingNumbers/728-SelfDividingNumbers.js
@@ -0,0 +1,14 @@
+            if(num[j]==0){
+        for(let j=0;j<num.length;j++){
+        let val;
+        let num=i.toString().split('').map(Number)
+    for(let i=left;i<=right;i++){
+    let arr=[]
+var selfDividingNumbers = function(left, right) {
+            val=false
+                break;
+            }else if(i%num[j]!=0){
+                break;
+            }else{
+                val=true
+1
