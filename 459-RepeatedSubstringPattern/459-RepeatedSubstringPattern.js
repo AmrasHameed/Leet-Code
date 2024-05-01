c3ab2c1238a3d5078162d4c2b9af7f7b914a28cb
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-01 10:21:02 +0530

    Create file

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.js
@@ -0,0 +1,17 @@
+            if (pattern === s) {
+            let sub = s.substring(0, factor);
+            let pattern = sub.repeat(len / factor);
+        if (len % factor === 0) {
+    for (let factor = 1; factor <= len / 2; factor++) {
+                return true;
+            }
+        }
+    }
+    return false;
+    let len = s.length;
+var repeatedSubstringPattern = function(s) {
+ */
+ * @return {boolean}
+ * @param {string} s
+};
+"
