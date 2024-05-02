f4de23dafd4a4c1cef9f1f7e08de3b42d2642b6b
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-02 19:08:48 +0530

    Create file

---
+++ 66-PlusOne/66-PlusOne.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    digits = digits.join('')
+    digits++
+    return digits.toString().split('')
+    digits = BigInt(digits)
+};
+[
