0eb6ecdf2e1ac1edc0c9d574fea5db4132f5f37c
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-04 10:04:25 +0530

    Create file

---
+++ 704-BinarySearch/704-BinarySearch.js
@@ -0,0 +1,17 @@
+            return mid
+        } 
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return -1
+        let mid = Math.floor((right + left) / 2)
+    while(left <= right) {
+    let right = nums.length - 1
+        if(target > nums[mid]) {
+        if(target === nums[mid]) {
+    let left = 0
+var search = function(nums, target) {
+};
+[
