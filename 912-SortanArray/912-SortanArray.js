9dca34df431f47b6a9ca7b375ffc000273db5df8
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-07 11:01:11 +0530

    Create file

---
+++ 912-SortanArray/912-SortanArray.js
@@ -0,0 +1,22 @@
+    const mid = Math.floor(nums.length / 2)
+    return merge(sortArray(left), sortArray(right))
+    let left = nums.slice(0, mid)
+    let right = nums.slice(mid)
+};
+
+function merge(left,right) {
+    while(left.length && right.length) {
+    let sorted = []
+        if(left[0] <= right[0]) {
+            sorted.push(left.shift())
+    }
+        return nums
+    if(nums.length < 2) {
+var sortArray = function(nums) {
+        } else {
+            sorted.push(right.shift())
+ */
+ * @return {number[]}
+        }
+    }
+[
