9be3c2ed62be2a31c1b5d0287762365e8531e750
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-07 17:55:07 +0530

    Create file

---
+++ 155-MinStack/155-MinStack.js
@@ -0,0 +1,17 @@
+    }
+    this.minStack.push(val)
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    this.minStack.pop()
+[
