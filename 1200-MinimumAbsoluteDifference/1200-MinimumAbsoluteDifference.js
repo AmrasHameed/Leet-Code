98a8c86910b44fb36f4193956771e9f2537cf65e
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-08 19:04:13 +0530

    Create file

---
+++ 1200-MinimumAbsoluteDifference/1200-MinimumAbsoluteDifference.js
@@ -0,0 +1,16 @@
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i + 1] - arr[i] == min) {
+    arr.sort((a,b) => a - b)
+    let min = arr[1] - arr[0]
+    }
+        if(min > arr[i + 1] - arr[i]) {
+    for(let i = 1; i < arr.length - 1; i++) {
+            min = arr[i + 1] - arr[i]
+        }
+var minimumAbsDifference = function(arr) {
+ */
+    let nums = []
+    console.log(min)
+        console.log(min)
+            nums.push([arr[i], arr[i + 1]])
+[
