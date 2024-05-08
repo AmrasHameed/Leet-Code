c0155ef0a734b2d123589241365dc5bb01fa16fd
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-08 18:44:27 +0530

    Create file

---
+++ 2255-CountPrefixesofaGivenString/2255-CountPrefixesofaGivenString.js
@@ -0,0 +1,13 @@
+    for(let i = 0; i < words.length; i++) {
+    }
+    let count = 0
+        if(s.startsWith(words[i])) {
+var countPrefixes = function(words, s) {
+ */
+ * @return {number}
+ * @param {string} s
+            count++
+        }
+    return count
+};
+[
