9dcca2f7a0210f935e9bcf4dd27d9a27f4cf1b97
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-10 00:49:05 +0530

    Create file

---
+++ 225-ImplementStackusingQueues/225-ImplementStackusingQueues.js
@@ -0,0 +1,16 @@
+    }
+    return this.q1[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q1.length === 0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+[
