8b2150a0d83aafe3ebae19fc52cee9c5a3398798
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-10 01:01:51 +0530

    Create file

---
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.js
@@ -0,0 +1,16 @@
+    }
+        return null
+    if(this.s1.length == 0) {
+MyQueue.prototype.pop = function() {
+ */
+ * @return {number}
+/**
+
+};
+    }
+        this.s1.push(this.s2.pop())
+    while(this.s2.length !== 0) {
+    this.s1.push(x)
+    }
+        this.s2.push(this.s1.pop())
+[
