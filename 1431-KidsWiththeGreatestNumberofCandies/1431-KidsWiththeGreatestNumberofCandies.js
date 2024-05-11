20a52ec747cc5aae02a59466c0b5f8805056704e
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-11 09:22:17 +0530

    Create file

---
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.js
@@ -0,0 +1,15 @@
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max= Math.max(...candies)
+        if(candies[i] + extraCandies >= max) {
+            arr.push(true)
+        } else {
+            arr.push(false)
+        }
+    }
+    return arr
+    let arr = []
+    for(let i = 0; i < candies.length; i++) {
+};
+[
