1a36283918e2e31956eae9cc8c90ffed466d5b5e
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-14 09:23:59 +0530

    Create file

---
+++ 450-DeleteNodeinaBST/450-DeleteNodeinaBST.js
@@ -0,0 +1,18 @@
+}
+    return min(root.left)
+    }
+        return root.val
+    if(!root.left) {
+var min = function(root) {
+ */
+ * @return {TreeNode}
+ * @param {number} key
+ * @param {TreeNode} root
+/**
+ */
+ * }
+
+var deleteNode = function(root, key) {
+    if(root == null) {
+        return root
+[
