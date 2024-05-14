cda1b9a8f2af8bf894f93def9086864225d555f9
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-14 09:55:25 +0530

    Create file

---
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.js
@@ -0,0 +1,14 @@
+        }
+            inOrder(root.left)
+            inOrder(root.right)
+            if(n++ < k) res = root.val
+    function inOrder(root) {
+        if(root) {
+    return res
+    }
+    let res;
+    let n = 0
+var kthSmallest = function(root, k) {
+    inOrder(root)
+ */
+[
