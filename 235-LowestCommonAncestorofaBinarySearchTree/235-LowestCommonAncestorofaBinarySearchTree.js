39b4804a436cadf5f3e0b2dddaa552700d9a1a71
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-14 16:04:31 +0530

    Create file

---
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.js
@@ -0,0 +1,13 @@
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q)
+    } else if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q)
+    } else {
+        return root
+    }
+[
