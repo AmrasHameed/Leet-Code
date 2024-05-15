9868dd334e7e39d569641c7beea86ff1cf3a7d89
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-15 12:11:55 +0530

    Create file

---
+++ 700-SearchinaBinarySearchTree/700-SearchinaBinarySearchTree.js
@@ -0,0 +1,13 @@
+        if (root.val == val) {
+            return root
+    while (root) {
+    }
+        return null
+    if (!root) {
+var searchBST = function (root, val) {
+ */
+ * @return {TreeNode}
+ * @param {number} val
+ * @param {TreeNode} root
+/**
+[
