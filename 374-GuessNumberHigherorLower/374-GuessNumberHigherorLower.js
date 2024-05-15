33f24243dc25a3c26920ee9dacef2d74ac2b12f7
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-15 18:03:38 +0530

    Create file

---
+++ 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.js
@@ -0,0 +1,16 @@
+        if(guess(mid)== 1) {
+            start = mid + 1
+        } else if(guess(mid)== -1) {
+            end = mid - 1
+        } else if(guess(mid)== 0) {
+            return mid
+    while(start <= end) {
+    let end = n
+        let mid = Math.floor((start + end)/2)
+        }
+    }
+};
+    let start = 1
+var guessNumber = function(n) {
+ */
+1
