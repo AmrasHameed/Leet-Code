c644658db5fed45653c6d014be8fd8689046f0e2
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-15 12:23:29 +0530

    Create file

---
+++ 701-InsertintoaBinarySearchTree/701-InsertintoaBinarySearchTree.js
@@ -0,0 +1,13 @@
+    if(root.val > node.val) {
+var insert = function(root, node) {
+};
+    }
+        insert(root, node)
+    } else {
+        root = node
+    if(!root) {
+    const node =new TreeNode(val)
+var insertIntoBST = function(root, val) {
+
+    return root
+[
