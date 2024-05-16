c582bbe6c42c0f518d51d783475d501e01a3d53c
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-16 09:41:07 +0530

    Update file 5/16/2024, 9:41:07 AM

--- 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.js
+++ 1732-FindtheHighestAltitude/1732-FindtheHighestAltitude.js
@@ -2,15 +2,15 @@
  * @param {number[]} gain
  * @return {number}
  */
-var largestAltitude = function(gain) {
-    for(let i = 0; i < gain.length; i++) {
-        sum += gain[i] 
+var largestAltitude = function (gain) {
+    for (let i = 0; i < gain.length; i++) {
+        sum += gain[i]
     let sum = 0
     let max = 0
-        if(sum > max) {
+        if (sum > max) {
             max = sum
         }
     }
     return max
-        console.log(sum)
+};
 [
