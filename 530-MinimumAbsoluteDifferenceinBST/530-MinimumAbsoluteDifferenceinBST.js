de44a17c2c49e08285f7ed3efeaa253d2f9a05c3
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-17 12:47:59 +0530

    Create file

---
+++ 530-MinimumAbsoluteDifferenceinBST/530-MinimumAbsoluteDifferenceinBST.js
@@ -0,0 +1,18 @@
+        }
+    }
+    inOrder(root)
+    for (let i = 0; i < arr.length - 1; i++) {
+        val = arr[i + 1] - arr[i]
+    let min = arr[1] - arr[0]
+        if (val < min) {
+            min = val
+        }
+            inOrder(root.right)
+            arr.push(root.val)
+            inOrder(root.left)
+        if (root) {
+    function inOrder(root) {
+    let arr = []
+var getMinimumDifference = function (root) {
+ */
+[
