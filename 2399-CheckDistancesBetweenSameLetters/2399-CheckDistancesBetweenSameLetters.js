69c04bd13bbfb855a432acc5c9afd6306dbce1d3
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-18 19:50:14 +0530

    Create file

---
+++ 2399-CheckDistancesBetweenSameLetters/2399-CheckDistancesBetweenSameLetters.js
@@ -0,0 +1,18 @@
+        let count = 0
+        while(j < s.length && s[j] !== s[i]) {
+            count++
+            j++
+        }
+        obj1[s[i]] = count
+    }
+    for(let i = 0; i < distance.length; i++) {
+        obj2[str[i]] = distance[i]
+    let str = "abcdefghijklmnopqrstuvwxyz"
+    console.log(obj1[str[i]], obj2[str[i]])
+    return true
+    }
+        }
+            return false
+        if(obj1[str[i]] !== undefined && obj1[str[i]] !== obj2[str[i]]) {
+};
+"
