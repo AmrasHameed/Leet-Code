e448be28c33f5c6ce5f512024116819f929c4e9b
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-20 01:20:16 +0530

    Update file 5/20/2024, 1:20:19 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.js
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.js
@@ -1,12 +1,17 @@
-        let largest = i
-    function minHeap(arr, n, i) {
-    }
-        minHeap(nums, nums.length, i)
-    for (let i = Math.floor(nums.length / 2) - 1; i >= 0; i--) {
-var findKthLargest = function (nums, k) {
- * @return {number}
- */
- * @param {number} k
 /**
  * @param {number[]} nums
-[
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function (nums, k) {
+    for (let i = Math.floor(nums.length / 2) - 1; i >= 0; i--) {
+        minHeap(nums, nums.length, i)
+    }
+    function minHeap(arr, n, i) {
+        let largest = i
+        let left = 2 * i + 1
+        let right = 2 * i + 2
+        if (right < n && arr[right] < arr[largest]) {
+            largest = right
+        }
+[3,2,1,5,6,4]
