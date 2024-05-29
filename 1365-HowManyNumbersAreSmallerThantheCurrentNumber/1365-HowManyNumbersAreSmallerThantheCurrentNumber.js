4cb66d747a031554bec46de2901adcb94e3f9c9d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-05-29 10:48:28 +0530

    Create file

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.js
@@ -0,0 +1,15 @@
+        for (let i = 0; i < nums.length; i++) {
+            let num = nums[j]
+            if (num > nums[i] && num !== nums[i]) {
+                count++
+            }
+        }
+        arr.push(count)
+    let arr = []
+    let j = 0
+var smallerNumbersThanCurrent = function (nums) {
+ */
+    while (j < nums.length) {
+        let count = 0
+        j++
+[
