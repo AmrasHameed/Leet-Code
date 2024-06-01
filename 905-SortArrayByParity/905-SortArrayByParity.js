4352970ee3c60d2d431bb4e86323de299b841c78
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-01 10:47:45 +0530

    Create file

---
+++ 905-SortArrayByParity/905-SortArrayByParity.js
@@ -0,0 +1,13 @@
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            arr.unshift(nums[i])
+        } else {
+            arr.push(nums[i])
+        }
+    }
+    let arr = []
+    return arr
+};
+var sortArrayByParity = function (nums) {
+ */
+[
