dc633b931f2791cd6296a4e993bfec8e4be0376d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-03 17:09:33 +0530

    Create file

---
+++ 896-MonotonicArray/896-MonotonicArray.js
@@ -0,0 +1,13 @@
+        } else if (nums[i] < nums[i - 1]) {
+        if (nums[i - 1] < nums[i]) {
+    for (let i = 1; i < nums.length; i++) {
+var isMonotonic = function (nums) {
+ */
+    }
+            minus = false
+            plus = false
+        }
+    return plus || minus
+    let plus = true, minus = true;
+};
+[
