4a26dee150264c10639a3d8955349286dffca2f1
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-04 21:18:45 +0530

    Create file

---
+++ 1476-SubrectangleQueries/1476-SubrectangleQueries.js
@@ -0,0 +1,14 @@
+
+/** 
+ * @param {number} row 
+ * @param {number} col
+ * @return {number}
+ */
+SubrectangleQueries.prototype.getValue = function (row, col) {
+    return this.rectangle[row][col]
+};
+
+/** 
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * var obj = new SubrectangleQueries(rectangle)
+[
