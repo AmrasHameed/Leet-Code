fbf8e62f0a7c31fe4ac121f228c342fc9651b59c
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-05 17:04:16 +0530

    Create file

---
+++ 2535-DifferenceBetweenElementSumandDigitSumofanArray/2535-DifferenceBetweenElementSumandDigitSumofanArray.js
@@ -0,0 +1,18 @@
+        if (num > 9) {
+    for (let i = 0; i < nums.length; i++) {
+    }
+        sum1 += nums[i]
+    let sum2 = 0
+    for (let i = 0; i < nums.length; i++) {
+var differenceOfSum = function (nums) {
+    let sum1 = 0
+ * @return {number}
+ */
+            num = num.toString().split('').reduce((acc, x) => acc + parseInt(x), 0);
+        }
+        sum2 += num
+    }
+    return Math.abs(sum1 - sum2)
+        let num = nums[i]
+};
+[
