2ab2799a8e83c8f3123cc3cdebe1092ae79822de
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-05 18:09:41 +0530

    Create file

---
+++ 283-MoveZeroes/283-MoveZeroes.js
@@ -0,0 +1,12 @@
+    for(let  i = 0; i < nums.length; i++) {
+        if(nums[i] !== 0) {
+        } 
+            [nums[i], nums[j]]= [nums[j], nums[i]]
+            j++
+    let j = 0
+var moveZeroes = function(nums) {
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ * @param {number[]} nums
+/**
+[
