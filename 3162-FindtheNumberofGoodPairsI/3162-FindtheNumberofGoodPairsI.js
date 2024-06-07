38deb9c6b0f342597a622aff3020085984d064de
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-07 19:50:27 +0530

    Create file

---
+++ 3162-FindtheNumberofGoodPairsI/3162-FindtheNumberofGoodPairsI.js
@@ -0,0 +1,12 @@
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] % (nums2[j] * k) == 0) {
+                count++
+            }
+        }
+    }
+    for (let i = 0; i < nums1.length; i++) {
+    let count = 0
+var numberOfPairs = function (nums1, nums2, k) {
+ */
+ * @return {number}
+[
