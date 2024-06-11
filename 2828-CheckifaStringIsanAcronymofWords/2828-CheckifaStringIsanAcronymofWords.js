f53a9c6c51346709c38337243dbeb21ef3f30ed5
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-11 11:18:52 +0530

    Create file

---
+++ 2828-CheckifaStringIsanAcronymofWords/2828-CheckifaStringIsanAcronymofWords.js
@@ -0,0 +1,11 @@
+ */
+var isAcronym = function (words, s) {
+    while (i < words.length) {
+        str += words[i][0]
+    }
+    let str = '';
+    return str === s;
+    let i = 0;
+        i++
+};
+[
