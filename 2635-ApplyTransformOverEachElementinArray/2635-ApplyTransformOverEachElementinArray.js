b6bd39fa169618397a3181fb3810786ef40760b8
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-14 17:33:43 +0530

    Create file

---
+++ 2635-ApplyTransformOverEachElementinArray/2635-ApplyTransformOverEachElementinArray.js
@@ -0,0 +1,13 @@
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    while (i < arr.length) {
+    }
+    return arr
+        arr[i] = fn(arr[i], i)
+ * @param {Function} fn
+ * @param {number[]} arr
+    let i = 0;
+        i++
+};
+f
