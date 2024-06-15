7ccf2d6c33da2fe00a5d718c09cf2602a752c002
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-15 12:45:20 +0530

    Create file

---
+++ 645-SetMismatch/645-SetMismatch.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+    for(let i = 0; i < nums.length; i++) {
+        arr[nums[i]]++;
+    }
+    return [arr.indexOf(2), arr.lastIndexOf(0)]
+    const arr = new Array(nums.length + 1).fill(0)
+[
