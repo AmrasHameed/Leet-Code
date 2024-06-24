a376defb5965d175a418a46d9eab66a0d923c757
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-06-24 23:11:03 +0530

    Create file

---
+++ 2114-MaximumNumberofWordsFoundinSentences/2114-MaximumNumberofWordsFoundinSentences.js
@@ -0,0 +1,11 @@
+var mostWordsFound = function (sentences) {
+    for (let i = 0; i < sentences.length; i++) {
+        let arr = sentences[i].split(' ')
+        if (arr.length > max) {
+    let max = 0
+            max = arr.length
+        }
+    }
+    return max
+};
+[
