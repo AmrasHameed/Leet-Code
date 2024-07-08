cbdadf7446eb563bfbdafbc01ce66902230b1d24
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-07-08 21:37:16 +0530

    Create file

---
+++ 2726-CalculatorwithMethodChaining/2726-CalculatorwithMethodChaining.js
@@ -0,0 +1,13 @@
+        return this
+        this.result = Math.pow(this.result, value)
+    power(value) {
+     */
+     * @return {Calculator}
+     * @param {number} value
+    /** 
+
+    }
+        return this
+    }
+
+[
