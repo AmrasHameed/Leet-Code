3702da72c0c247fea50f69a35bb006ab570e26f8
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-07-10 19:47:28 +0530

    Update file 7/10/2024, 7:47:28 PM

--- 169-MajorityElement/169-MajorityElement.js
+++ 169-MajorityElement/169-MajorityElement.js
@@ -1,12 +1,15 @@
         }
-    }
-    for (let val in obj) {
-        if (obj[val] > nums.length / 2) {
-            return val
-        }
-    }
-};
+        if (obj[nums[i]] > max) {
+            ans = nums[i]
             obj[nums[i]] = 1;
         } else {
             obj[nums[i]]++
+        if (obj[nums[i]]) {
+    for (let i = 0; i < nums.length; i++) {
+    let ans
+            max = obj[nums[i]]
+        }
+    }
+    return ans
+};
 [
