33894b6fea76004ef699bc1cc2e90c17eebed8f5
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-07-10 19:42:49 +0530

    Create file

---
+++ 169-MajorityElement/169-MajorityElement.js
@@ -0,0 +1,12 @@
+        }
+    }
+    for (let val in obj) {
+        if (obj[val] > nums.length / 2) {
+            return val
+        }
+    }
+};
+            obj[nums[i]] = 1;
+        } else {
+            obj[nums[i]]++
+[
