e06df83d10f1b372100790777b8960a668267c3c
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-07-17 22:57:18 +0530

    Create file

---
+++ 1859-SortingtheSentence/1859-SortingtheSentence.js
@@ -0,0 +1,14 @@
+var sortSentence = function (s) {
+    s = s.split(' ')
+    for (let i = 0; i < s.length; i++) {
+        let val = s[i][s[i].length - 1]
+    }
+        arr[val] = s[i].substring(0, s[i].length - 1)
+    let arr = []
+    return arr.join(' ').trim()
+};
+ */
+ * @return {string}
+ * @param {string} s
+/**
+"
