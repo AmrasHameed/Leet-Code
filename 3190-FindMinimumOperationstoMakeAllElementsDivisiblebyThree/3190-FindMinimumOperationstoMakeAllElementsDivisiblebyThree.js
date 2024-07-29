7b594466d465f366d82cab840deb8830644ba752
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-07-29 22:52:57 +0530

    Create file

---
+++ 3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree/3190-FindMinimumOperationstoMakeAllElementsDivisiblebyThree.js
@@ -0,0 +1,22 @@
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function (nums) {
+    for (let num of nums) {
+        if (num % 3 !== 0) {
+    let count = 0
+            count++
+        }
+    }
+    return count
+};
+[1,2,3,4]
+[3,6,9]
+[5,7,3,8]
+3
+0
+3
+3
+0
+3
+[
