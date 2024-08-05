e7ae48b1c1991cb57302408089a78d39fdc1285d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-05 11:24:40 +0530

    Create file

---
+++ 2053-KthDistinctStringinanArray/2053-KthDistinctStringinanArray.js
@@ -0,0 +1,23 @@
+        }
+    }
+    for (let val in obj) {
+        if (obj[val] === 1) {
+    }
+            newArr.push(val)
+        }
+    return newArr[k - 1] ? newArr[k - 1] : ""
+    let newArr = []
+};
+["d","b","c","b","c","a"]
+2
+["aaa","aa","a"]
+1
+["a","b","a"]
+3
+"a"
+"aaa"
+""
+"a"
+"aaa"
+""
+[
