4ed9bad6ef427678ac807b6c7bdbd866c1a5c100
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-09 00:16:52 +0530

    Create file

---
+++ 1656-DesignanOrderedStream/1656-DesignanOrderedStream.js
@@ -0,0 +1,20 @@
+        this.arr = [];
+    }
+
+    insert(id, val) {
+        let chunk = [];
+        this.arr[id - 1] = val;
+        while (this.arr[this.value]) {
+            chunk.push(this.arr[this.value]);
+            this.value++;
+        }
+        return chunk;
+    }
+        this.value = 0;
+    constructor(n) {
+class OrderedStream {
+["OrderedStream","insert","insert","insert","insert","insert"]
+[[5],[3,"ccccc"],[1,"aaaaa"],[2,"bbbbb"],[5,"eeeee"],[4,"ddddd"]]
+[null,[],["aaaaa"],["bbbbb","ccccc"],[],["ddddd","eeeee"]]
+[null,[],["aaaaa"],["bbbbb","ccccc"],[],["ddddd","eeeee"]]
+[
