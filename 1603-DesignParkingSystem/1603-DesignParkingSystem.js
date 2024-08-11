1170010d442fec940a4176fcb11811298c70b847
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-11 23:00:08 +0530

    Create file

---
+++ 1603-DesignParkingSystem/1603-DesignParkingSystem.js
@@ -0,0 +1,20 @@
+    if (carType == 2) this.medium--;
+    if (carType == 1) this.big--;
+    }
+    if ((carType === 1 && this.big === 0) || (carType == 2 && this.medium == 0) || (carType == 3 && this.small == 0)) {
+        return false
+ */
+ParkingSystem.prototype.addCar = function (carType) {
+ * @return {boolean}
+/** 
+ * @param {number} carType
+
+};
+    this.small = small
+    this.medium = medium
+    this.big = big
+["ParkingSystem","addCar","addCar","addCar","addCar"]
+[[1,1,0],[1],[2],[3],[1]]
+[null,true,true,false,false]
+[null,true,true,false,false]
+[
