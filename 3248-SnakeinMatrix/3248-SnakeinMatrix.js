24367a3388abbf074e6307dab429dfa9b813995e
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-17 19:54:47 +0530

    Create file

---
+++ 3248-SnakeinMatrix/3248-SnakeinMatrix.js
@@ -0,0 +1,23 @@
+            val++
+        } else if (command === 'LEFT') {
+            val--
+        } else if (command === 'UP') {
+            val -= n
+        } else {
+        if (command === 'RIGHT') {
+            val += n
+        }
+    return val
+    }
+    for (let command of commands) {
+    let val = 0
+};
+2
+["RIGHT","DOWN"]
+3
+["DOWN","RIGHT","UP"]
+3
+1
+3
+1
+2
