96ccbd83abea474b7f0a4fecf7f932e3421b52c4
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-19 23:05:29 +0530

    Create file

---
+++ 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.js
@@ -0,0 +1,23 @@
+ */
+var canAliceWin = function (nums) {
+    for (let num of nums) {
+        if (num >= 10) {
+            sum1 += num
+        } else {
+    }
+    let sum1 = 0
+    let sum2 = 0
+            sum2 += num
+        }
+    return !(sum1 === sum2)
+};
+[1,2,3,4,10]
+[1,2,3,4,5,14]
+[5,5,5,25]
+false
+true
+true
+false
+true
+true
+[
