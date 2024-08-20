abdfdb36a338228f8b06b3abd8e29eabd47039f5
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-21 00:26:03 +0530

    Create file

---
+++ 3120-CounttheNumberofSpecialCharactersI/3120-CounttheNumberofSpecialCharactersI.js
@@ -0,0 +1,27 @@
+        }
+    }
+        } else if (char >= 'A' && char <= 'Z') {
+            uppercaseSet.add(char);
+    let specialCount = 0;
+    for (let char of lowercaseSet) {
+        if (uppercaseSet.has(char.toUpperCase())) {
+            specialCount++;
+        }
+            lowercaseSet.add(char);
+        if (char >= 'a' && char <= 'z') {
+    for (let char of word) {
+    
+    let uppercaseSet = new Set();
+"aaAbcBC"
+"abc"
+"abBCab"
+"BBbab"
+3
+0
+1
+1
+3
+0
+1
+1
+"
