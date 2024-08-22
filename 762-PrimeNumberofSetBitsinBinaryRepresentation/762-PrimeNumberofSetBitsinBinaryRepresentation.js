82a49aa3690ee8a577047fc6b489720ca2180502
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-22 16:46:30 +0530

    Create file

---
+++ 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.js
@@ -0,0 +1,22 @@
+    if (num % 2 === 0 || num % 3 === 0) {
+    }
+        return true;
+    if (num <= 3) {
+    }
+        return false;
+    if (num <= 1) {
+function isPrime(num) {
+ */
+ * @return {number}
+ * @param {number} right
+ * @param {number} left
+/**
+6
+10
+10
+15
+4
+5
+4
+5
+6
