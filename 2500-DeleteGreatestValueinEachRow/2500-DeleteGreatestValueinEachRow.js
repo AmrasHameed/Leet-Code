4a6cd89643cdcde0793dee8b40f5d6b436e783ee
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-26 00:00:44 +0530

    Create file

---
+++ 2500-DeleteGreatestValueinEachRow/2500-DeleteGreatestValueinEachRow.js
@@ -0,0 +1,21 @@
+                }
+                let index = grid[i].indexOf(num);
+                if (index !== -1) {
+                    grid[i].splice(index, 1);
+                    max = num;
+                if (num > max) {
+                let num = Math.max(...grid[i]);
+            if (grid[i].length > 0) {
+        for (let i = 0; i < grid.length; i++) {
+        let max = -Infinity;
+    while (grid.length) {
+    let ans = 0;
+var deleteGreatestValue = function (grid) {
+ */
+[[1,2,4],[3,3,1]]
+[[10]]
+8
+10
+8
+10
+[
