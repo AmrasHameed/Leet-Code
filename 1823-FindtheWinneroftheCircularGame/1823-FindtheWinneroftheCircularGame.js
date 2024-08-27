a74589a3bb60e5b3b12ce1b75de7191e83ae77f2
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-27 11:55:47 +0530

    Create file

---
+++ 1823-FindtheWinneroftheCircularGame/1823-FindtheWinneroftheCircularGame.js
@@ -0,0 +1,22 @@
+        arr.push(i)
+    }
+        arr.splice(index, 1);
+    while (arr.length !== 1) {
+    let index = 0
+    }
+        index = (index + k - 1) % arr.length;
+    return arr[0]
+    for (let i = 1; i <= n; i++) {
+    let arr = []
+var findTheWinner = function (n, k) {
+ */
+};
+5
+2
+6
+5
+3
+1
+3
+1
+5
