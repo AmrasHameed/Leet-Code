b062c8de912e273f2a0c192eb539593d8cb8d730
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-08-28 12:39:15 +0530

    Create file

---
+++ 1051-HeightChecker/1051-HeightChecker.js
@@ -0,0 +1,23 @@
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
+    let count = 0
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== arr[i]) {
+            count++
+        }
+    arr.sort((a, b) => a - b)
+    let arr = [...heights]
+    }
+    return count
+[1,1,4,2,1,3]
+[5,1,2,3,4]
+[1,2,3,4,5]
+3
+5
+0
+3
+5
+0
+[
