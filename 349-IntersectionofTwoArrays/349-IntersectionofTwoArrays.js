c976af0fd37d5024e244edba3944b69afc9e1211
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-04 09:35:31 +0530

    Create file

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.js
@@ -0,0 +1,26 @@
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+    let arr = []
+    nums2 = [...new Set(nums2)]
+    nums1 = [...new Set(nums1)]
+var intersection = function (nums1, nums2) {
+ */
+ * @return {number[]}
+            arr.push(nums1[i])
+        }
+    }
+    return arr
+};
+[1,2,2,1]
+[2,2]
+[4,9,5]
+[9,4,9,8,4]
+[4,7,9,7,6,7]
+[5,0,0,6,1,6,2,2,4]
+[2]
+[4,9]
+[4,6]
+[2]
+[9,4]
+[6,4]
+[
