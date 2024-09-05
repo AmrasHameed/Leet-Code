b68ae16c994a711e2fc4633ee55cc0e86991f9e2
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-05 13:38:58 +0530

    Create file

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.js
@@ -0,0 +1,23 @@
+    let min = prices[0]
+    for (let i = 1; i < prices.length; i++) {
+        if (min > prices[i]) {
+            min = prices[i]
+        }
+    }
+    let amt = 0
+    return amt
+var maxProfit = function (prices) {
+        amt = Math.max(amt, prices[i] - min)
+};
+ */
+ * @return {number}
+[7,1,5,3,6,4]
+[7,6,4,3,1]
+[2,4,1]
+5
+0
+2
+5
+0
+2
+[
