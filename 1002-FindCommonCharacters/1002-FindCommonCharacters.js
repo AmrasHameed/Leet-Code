362685b49550b9c5ac5392ae9b5ba9dab8f0f660
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-13 20:45:49 +0530

    Create file

---
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.js
@@ -0,0 +1,18 @@
+    return arr
+        if (words.every((word) => word.includes(x))) {
+    for (let x of words[0]) {
+    let arr = [];
+var commonChars = function (words) {
+ */
+            arr.push(x)
+    }
+            words = words.map((word) => word.replace(x, ""))
+        }
+};
+["bella","label","roller"]
+["cool","lock","cook"]
+["e","l","l"]
+["c","o"]
+["e","l","l"]
+["c","o"]
+[
