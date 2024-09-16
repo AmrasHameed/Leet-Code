0d9712e95523935ae79dc212740428b7632cce82
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-16 23:43:08 +0530

    Create file

---
+++ 202-HappyNumber/202-HappyNumber.js
@@ -0,0 +1,20 @@
+        }
+        seen.add(result);
+            return false;
+        if (seen.has(result)) {
+        result = arr.map(Number).reduce((acc, x) => acc + (x * x), 0);
+        let arr = result.toString().split('');
+    while (result !== 1) {
+
+    }
+
+    return true;
+};
+
+19
+2
+true
+false
+true
+false
+1
