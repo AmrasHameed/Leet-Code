3f594acb378cc242ef23f7c4505bace19560521a
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-17 23:40:16 +0530

    Create file

---
+++ 2824-CountPairsWhoseSumisLessthanTarget/2824-CountPairsWhoseSumisLessthanTarget.js
@@ -0,0 +1,11 @@
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]<target){
+                count++;
+            }
+        }
+    }
+    return count
+};
+[-1,1,2,3,1]
