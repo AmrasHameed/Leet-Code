37bf2e00ae3797496b27036e9ebfe5e9dd439574
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-09-25 12:31:12 +0530

    Create file

---
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.js
@@ -0,0 +1,17 @@
+    for(let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            result += arr[j];
+            j++;
+        } else {
+            result += s[i]; 
+        }
+    }
+    return result
+};
+"IceCreAm"
+"leetcode"
+"AceCreIm"
+"leotcede"
+"AceCreIm"
+"leotcede"
+"
