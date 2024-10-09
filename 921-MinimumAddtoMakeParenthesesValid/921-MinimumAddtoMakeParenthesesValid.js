70deae30366d34662c9b595f8c908c3b25c828b9
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-10-09 21:36:44 +0530

    Create file

---
+++ 921-MinimumAddtoMakeParenthesesValid/921-MinimumAddtoMakeParenthesesValid.js
@@ -0,0 +1,23 @@
+            count++
+        } else {
+            count--
+        }
+        if (count < 0) {
+            count1++
+        if (s[i] === '(') {
+    for (let i = 0; i < s.length; i++) {
+    let count1 = 0
+    let count = 0
+            count = 0
+        }
+    }
+"())"
+"((("
+"())("
+1
+3
+2
+1
+3
+2
+"
