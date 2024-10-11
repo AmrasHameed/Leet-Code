a760f857e8faff8550952b104154195640813c97
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-10-11 14:55:51 +0530

    Create file

---
+++ 747-LargestNumberAtLeastTwiceofOthers/747-LargestNumberAtLeastTwiceofOthers.js
@@ -0,0 +1,20 @@
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== m && m < (2 * nums[i])) {
+            return -1
+        }
+    }
+    return maxIndex
+};
+        }
+            maxIndex = i
+            m = nums[i]
+        if (nums[i] > m) {
+    for (let i = 0; i < nums.length; i++) {
+[3,6,1,0]
+[1,2,3,4]
+1
+-1
+1
+-1
+[
