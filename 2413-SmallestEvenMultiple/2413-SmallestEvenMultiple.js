49d8e7ff0568910edc9fd0387fa29e1e651bf4d9
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2024-10-15 10:32:44 +0530

    Create file

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function (n) {
+    if (n >= 2 && n % 2 === 0) {
+        return n
+    } else {
+        return n * 2
+    }
+};
+5
+6
+2
+10
+6
+2
+10
+6
+2
+5
