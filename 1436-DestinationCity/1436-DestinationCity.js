ca21992c87e59638e99fd10c1491c273cfac4617
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-20 14:02:34 +0530

    Update file 5/20/2025, 2:02:35 PM

--- 1436-DestinationCity/1436-DestinationCity.js
+++ 1436-DestinationCity/1436-DestinationCity.js
@@ -1,12 +1,16 @@
-    }
-            arrev.push(arr[i])
         }else {
+            arrev.push(arr[i])
+        if(i%2===0) {
+    for(let i = 0; i < arr.length; i++) {
+    let arrod = [], arrev = [];
             arrod.push(arr[i])
         }
-    console.log(arrev,arrod)
     }
-        }
-            return arrod[i]
-        if(!arrev.includes(arrod[i])) {
+    let arr = paths.flat(2)
+var destCity = function (paths) {
+ */
+ * @return {string}
+ * @param {string[][]} paths
+/**
+    console.log(arrev,arrod)
     for(let i = 0; i < arrod.length; i++) {
-};
