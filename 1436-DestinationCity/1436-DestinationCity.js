0e73bd873abc1a8e392e8068edd2e831c4754520
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-20 13:08:47 +0530

    Create file

---
+++ 1436-DestinationCity/1436-DestinationCity.js
@@ -0,0 +1,12 @@
+    }
+            arrev.push(arr[i])
+        }else {
+            arrod.push(arr[i])
+        }
+    console.log(arrev,arrod)
+    }
+        }
+            return arrod[i]
+        if(!arrev.includes(arrod[i])) {
+    for(let i = 0; i < arrod.length; i++) {
+};
