ff750bc7dc567c4765bccfc3eacc1212de052ec4
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-23 16:35:56 +0530

    Create file

---
+++ 2733-NeitherMinimumnorMaximum/2733-NeitherMinimumnorMaximum.js
@@ -0,0 +1,12 @@
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+        if (num !== min && num !== max) {
+            return num;
+        }
+    for (let num of nums) {
+ */
+var findNonMinOrMax = function (nums) {
+    }
+    return -1;
+
+};
