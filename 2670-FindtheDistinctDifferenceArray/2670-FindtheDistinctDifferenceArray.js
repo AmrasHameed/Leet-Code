b9792eb850d5ede21d46926b1b81536f8e2e8779
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-24 11:42:02 +0530

    Create file

---
+++ 2670-FindtheDistinctDifferenceArray/2670-FindtheDistinctDifferenceArray.js
@@ -0,0 +1,10 @@
+    for(let i = 0 ; i < nums.length; i++) {
+        let arr1 = nums.slice(0, i +1)
+    }
+        let arr2 = nums.slice(i + 1, nums.length)
+        arr2 = [...new Set(arr2)].length
+    let arr = []
+    return arr
+        arr1 = [...new Set(arr1)].length
+        arr.push(arr1 - arr2)
+};
