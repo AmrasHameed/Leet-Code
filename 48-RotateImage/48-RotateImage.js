f9764ca53fef31b684f3eab7a18f76dd540f6dc8
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-26 16:13:37 +0530

    Create file

---
+++ 48-RotateImage/48-RotateImage.js
@@ -0,0 +1,13 @@
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+        }
+    }
+            arr[j][n - i - 1] = matrix[i][j]
+    for (let i = 0; i < n; i++) {
+    let arr = Array.from({ length: n }, () => Array(n).fill(0));
+        for (let j = 0; j < n; j++) {
+            matrix[i][j] = arr[i][j];
+        }
+    }
+    let n = matrix.length
+var rotate = function (matrix) {
