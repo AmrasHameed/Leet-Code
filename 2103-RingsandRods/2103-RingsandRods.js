58e09156cb06af14d1e4c22f001ed48f5a50bfc1
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-05-31 13:13:13 +0530

    Create file

---
+++ 2103-RingsandRods/2103-RingsandRods.js
@@ -0,0 +1,15 @@
+        if(!obj[rings[i + 1]]) {
+    }
+    let count = 0;
+    for(let val in obj) {
+        if(obj[val].has('R')&&obj[val].has('G')&&obj[val].has('B')) {
+            obj[rings[i + 1]] = new Set()
+        }
+        obj[rings[i + 1]].add(rings[i])
+    for(let i = 0; i < rings.length; i+=2) {
+    let obj = {}
+            count++
+        }
+    }
+    return count
+};
