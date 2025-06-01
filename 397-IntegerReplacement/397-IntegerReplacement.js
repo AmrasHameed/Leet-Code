5445f3b085ed953c345a4e91f9cb873c8ef3e46d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-01 15:47:33 +0530

    Create file

---
+++ 397-IntegerReplacement/397-IntegerReplacement.js
@@ -0,0 +1,19 @@
+            count++
+        if (n % 2 === 0) {
+    while (n !== 1) {
+    let count = 0;
+var integerReplacement = function (n) {
+ */
+            n = n / 2
+        } else {
+            if (((n - 1) / 2) % 2 === 0 || n === 3) {
+                n = n - 1
+                count++
+            } else {
+                n = n + 1
+                count++
+            }
+        }
+    }
+ * @return {number}
+ * @param {number} n
