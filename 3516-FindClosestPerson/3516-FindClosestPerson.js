eb4f106f78f0de1b2b066966a5521ebb5a74021c
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-03 16:07:47 +0530

    Create file

---
+++ 3516-FindClosestPerson/3516-FindClosestPerson.js
@@ -0,0 +1,10 @@
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function (x, y, z) {
+    if(Math.abs(z - x) === Math.abs(z - y)){
+        return 0
+    }
+    return Math.abs(z - x) < Math.abs(z - y) ? 1 : 2;
+};
