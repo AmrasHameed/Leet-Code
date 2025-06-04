8d9230f2e49854ef976121c3cc1bf071b970f61f
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-04 13:13:04 +0530

    Create file

---
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.js
@@ -0,0 +1,10 @@
+    for(let  i = 1; i <= nums.length; i++) {
+        if(!set.has(i)) {
+            arr.push(i)
+        }
+    }
+    let arr = []
+    return arr
+    let set = new Set(nums)
+var findDisappearedNumbers = function (nums) {
+ */
