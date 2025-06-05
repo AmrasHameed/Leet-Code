f7bf825f769e108b8d8752da8973d90b2c770df2
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-05 18:28:39 +0530

    Create file

---
+++ 3211-GenerateBinaryStringsWithoutAdjacentZeros/3211-GenerateBinaryStringsWithoutAdjacentZeros.js
@@ -0,0 +1,10 @@
+const validStrings = function (n) {
+ */
+ * @return {string[]}
+ * @param {number} n
+    if(n === 1) {
+        return ['0','1']
+    }
+    if(n === 2) {
+        return ['01','10', '11']
+    }
