ee8b700b816cec24092c16cde959d13b5a06ba5d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-07 13:55:16 +0530

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.js
@@ -0,0 +1,10 @@
+            word += word1[i];
+        }
+        if (i < word1.length) {
+    for (let i = 0; i < len; i++) {
+    len = Math.max(word1.length, word2.length)
+    let word = ''
+var mergeAlternately = function (word1, word2) {
+ */
+ * @return {string}
+ * @param {string} word2
