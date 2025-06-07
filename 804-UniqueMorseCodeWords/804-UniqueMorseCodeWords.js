dee34365d1636288a3e7867c48e24e94cf85be6a
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-07 13:45:36 +0530

    Create file

---
+++ 804-UniqueMorseCodeWords/804-UniqueMorseCodeWords.js
@@ -0,0 +1,10 @@
+    for(let word of words) {
+    }
+        map.set(letter, arr[i])
+        const letter = String.fromCharCode(97 + i);
+    for(let i = 0; i <= 26; i++) {
+    const set = new Set();
+    let arr = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    const map = new Map()
+var uniqueMorseRepresentations = function(words) {
+ */
