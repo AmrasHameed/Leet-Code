0cc88c70615f2665cbb229f6a385111aca78f382
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-08 14:06:50 +0530

    Create file

---
+++ 2678-NumberofSeniorCitizens/2678-NumberofSeniorCitizens.js
@@ -0,0 +1,10 @@
+var countSeniors = function(details) {
+    for(let str of details) {
+        str = str.split('').slice(11).map(Number).join('')
+        if(str >= 6100) {
+            count++
+    let count = 0
+        }
+    }
+    return count
+};
