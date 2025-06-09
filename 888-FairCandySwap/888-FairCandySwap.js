961e440abdfc641c9543d92662032c22bbfb013d
AmrasHameed <152363620+AmrasHameed@users.noreply.github.com>
2025-06-09 20:09:49 +0530

    Create file

---
+++ 888-FairCandySwap/888-FairCandySwap.js
@@ -0,0 +1,11 @@
+    for(let num1 of aliceSizes) {
+    let diff = (sum1 - sum2) / 2
+    let sum2 = bobSizes.reduce((acc,x)=>acc+x,0)
+    let sum1 = aliceSizes.reduce((acc,x)=>acc+x,0)
+var fairCandySwap = function(aliceSizes, bobSizes) {
+ */
+ * @return {number[]}
+ * @param {number[]} bobSizes
+ * @param {number[]} aliceSizes
+        for(let num2 of bobSizes) {
+            if(num1 - num2 === diff) {
